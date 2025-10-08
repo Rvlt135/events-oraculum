db8c21074d2cb3b5c59e2f4ae5077de120a2df0e
Layerbiter <ivaninrvlt@gmail.com>
2025-10-08 23:01:10 +0200

    Updated README.md

--- odds-service/app/tasks/broker.py
+++
@@ -1,5 +0,0 @@
-from taskiq_redis import RedisAsyncResultBackend, ListQueueBroker
-from app.config import settings
-
-redis_backend = RedisAsyncResultBackend(settings.redis_url)
-broker = ListQueueBroker(url=settings.redis_url).with_result_backend(redis_backend)
