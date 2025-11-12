c91bca00a7c0b9c97a21aa304b600a93093e96ac
Valentin Bashkatov <bashkatovvala@gmail.com>
2025-11-12 21:19:30 +0100

    Add cookies creation feature

---
+++ gateway-service/app/services/cookies.py
@@ -0,0 +1,26 @@
+from fastapi import Response
+
+from app.config.settings import settings
+
+
+def set_auth_cookies(response: Response, access_token: str, refresh_token: str) -> None:
+    print("_________________ACS:", settings.auth_cookie_secure)
+    response.set_cookie(
+        key=settings.access_token_cookie_name,
+        value=access_token,
+        httponly=True,
+        secure=settings.auth_cookie_secure,
+        samesite="Lax",
+        path="/",
+        max_age=settings.auth_access_ttl_sec,
+    )
+
+    response.set_cookie(
+        key=settings.refresh_token_cookie_name,
+        value=refresh_token,
+        httponly=True,
+        secure=settings.auth_cookie_secure,
+        samesite="Strict",
+        path="/",
+        max_age=settings.auth_refresh_ttl_sec,
+    )
