caa70a18126f1e84edb13c10fd483bfdf733c812
Valentin Bashkatov <bashkatovvala@gmail.com>
2025-11-24 09:56:54 +0100

    Update dependecies

--- gateway-service/app/api/di/deps.py
+++ gateway-service/app/api/di/deps.py
@@ -3,11 +3,11 @@ from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.config.settings import Settings, settings
-from app.infrastructure.clients.telegram_validator import TelegramValidator
-from app.infrastructure.cache.redis import RedisCache
 from app.infrastructure.cache.oauth_cache import OauthCache
+from app.infrastructure.cache.redis import RedisCache
 from app.infrastructure.cache.session_cache import SessionCache
 from app.infrastructure.cache.user_cache import UserCache
+from app.infrastructure.clients.telegram_validator import TelegramValidator
 from app.infrastructure.db.session import get_session
 from app.infrastructure.security.password import PasswordService
 from app.services.auth_service import EmailAuthService, GoogleAuthService, TokenService
@@ -70,13 +70,15 @@ async def get_google_auth_service(
 
 async def get_token_service(
     db: AsyncSession = Depends(get_session),
+    session_cache: SessionCache = Depends(get_session_cache),
+    user_cache: UserCache =  Depends(get_user_cache)
 ) -> TokenService:
-    return TokenService(db_session=db)
+    return TokenService(db_session=db, session_cache=session_cache, user_cache=user_cache)
 
-# TODO - remove/update this method with the refactoring AuthService 
-async def get_auth_service(
+
+async def get_email_auth_service(
     db: AsyncSession = Depends(get_session),
     user_cache: UserCache =  Depends(get_user_cache),
-    session_cashe: SessionCache = Depends(get_session_cache)
+    session_cashe: SessionCache = Depends(get_session_cache),
 ) -> EmailAuthService:
     return EmailAuthService(db_session=db, user_cache=user_cache, session_cache=session_cashe)
