e16916848c885010a4b691fe453108b057ee2bdd
Valentin Bashkatov <bashkatovvala@gmail.com>
2025-11-26 16:30:08 +0100

    Add endpoints for Ivite code settings

--- gateway-service/app/api/routes/base.py
+++ gateway-service/app/api/routes/base.py
@@ -3,14 +3,16 @@ from datetime import UTC, datetime
 from fastapi import APIRouter, Depends, Query
 
 from app.api.di.auth_deps import get_current_user
-from app.infrastructure.db.session import get_session
+from app.api.di.deps import get_settings_cache
+from app.api.schemas.common import InviteSettingUpdate
 from app.api.schemas.user import (
     MeResponse,
     UserProfile,
 )
+from app.infrastructure.cache.settings_cache import SettingCache
 from app.infrastructure.db.orm.user import User
-from app.infrastructure.db.orm.invite_code import InviteCode
 from app.infrastructure.db.repositories.invite_code_repo import InviteCodeRepository
+from app.infrastructure.db.session import get_session
 
 router = APIRouter()
 
@@ -33,6 +35,35 @@ async def get_me(
     )
     return resp.model_dump(exclude_none=True) # TODO: убрать null в будущем в ответе
 
+
+@router.put("/settings/invite_code_required")
+async def update_invite_code_setting(
+    req: InviteSettingUpdate,
+    settings_cache: SettingCache = Depends(get_settings_cache),
+    # TODO: add admin dependency
+) -> dict:
+    await settings_cache.set_setting_cache(
+        key="invite_code_required",
+        value="true" if req.invite_code_required else "false",
+    )
+    return {
+        "message": "Invite code requirement updated",
+        "invite_code_required": req.invite_code_required,
+    }
+
+
+@router.get("/settings/invite_code_required")
+async def get_invite_code_setting(
+    settings_cache: SettingCache = Depends(get_settings_cache),
+    # TODO: add admin dependency
+) -> InviteSettingUpdate:
+    value = await settings_cache.get_setting_cache(
+        key="invite_code_required",
+    )
+    invite_code_required = value.lower() == "true"
+    return InviteSettingUpdate(
+        invite_code_required=invite_code_required,
+    )
 
 # Dummy endpoints for development
 
